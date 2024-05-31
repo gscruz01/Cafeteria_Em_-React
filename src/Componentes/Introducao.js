b6a10dfd21bae6afce1b9725c08ebb959e8986d6
Guilherme <gcsantos2812@gmail.com>
2024-05-31 10:09:36 -0300

    Aplicando Responsividade na Introdução 50% concluido

--- src/Componentes/Introducao.js
+++ src/Componentes/Introducao.js
@@ -1,12 +1,11 @@
 import React,  {Component} from 'react';
 import '../../src/App.css';
-import FotoCapa from "../img/Foto-Capa.png"
 
 class Introducao extends Component {
     render(){
         return(
             <div className='Introducao'>
-                <img src={FotoCapa} alt='Foto de Capa' title='Foto de Capa' id='imgfundo'/>
+                <div className='imgfundo'><img/></div>
                 <h1>Coffee <span>all day</span></h1>
                 <p>Bem-vindo ao Coffee All Day, o seu refúgio acolhedor para os amantes de café! Aqui, cada xícara é uma
                     experiência única, onde o aroma rico e os sabores intensos se combinam para criar momentos
